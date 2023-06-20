ce12c73d2c73fb2cc9e71f9e9c7e4ed72af31e47
webinartelran <webinartelran@gmail.com>
2023-06-20 19:35:32 +0300

    CW8_Chain

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,5 +1,6 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HelperUser extends HelperBase{
@@ -8,6 +9,22 @@ public class HelperUser extends HelperBase{
     public HelperUser(WebDriver wd) {
         super(wd);
     }
+
+    public void openLoginForm(){
+        wd.findElement(By.xpath("//*[.=' Log in ']")).click();
+    }
+
+    public void fillLoginForm(String email, String password){
+        type(By.xpath("//input[@id='email']"), email);
+        type(By.xpath("//input[@id='password']"), password);
+    }
+
+    public void submitLogin(){
+
+        wd.findElement(By.xpath("//button[@type='submit']")).submit();
+    }
+
+
 
 
 }
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,10 +1,27 @@
+import models.User;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
 
     @Test
     public void loginPositive(){
-        
+
+    app.getUser().openLoginForm();
+        app.getUser().fillLoginForm("asd@fgh.com","$Asdf1234");
+        app.getUser().submitLogin();
+
+    }
+    @Test
+    public void loginPositiveUser(){
+//    User user = new User("asd@fgh.com","$Asdf1234");
+        User user = new User();
+//        user.setEmail("asd@fgh.com");
+//        user.setPassword("$Asdf1234");
+        user.setEmail("");
+    app.getUser().openLoginForm();
+        app.getUser().fillLoginForm(user.getEmail(), user.getPassword());
+        app.getUser().submitLogin();
+
     }
 
 }
