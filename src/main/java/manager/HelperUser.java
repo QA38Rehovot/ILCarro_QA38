6b993e0d3113bbaaa4124d166eced052273c61f2
webinartelran <webinartelran@gmail.com>
2023-06-20 20:30:08 +0300

    CW8

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,5 +1,6 @@
 package manager;
 
+import models.User;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -18,13 +19,30 @@ public class HelperUser extends HelperBase{
         type(By.xpath("//input[@id='email']"), email);
         type(By.xpath("//input[@id='password']"), password);
     }
+//    public void fillLoginForm(String password, String email){
+//        type(By.xpath("//input[@id='email']"), email);
+//        type(By.xpath("//input[@id='password']"), password);
+//    }
 
+    // overloading
+    public void fillLoginForm(User user){
+        type(By.xpath("//input[@id='email']"), user.getEmail());
+        type(By.xpath("//input[@id='password']"), user.getPassword());
+    }
+
+    // method signature - type + name + parameters types
     public void submitLogin(){
 
         wd.findElement(By.xpath("//button[@type='submit']")).submit();
     }
 
+    public void logout(){
 
+    }
+
+    public boolean isLogged(){
+
+    }
 
 
 }
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,7 +1,15 @@
 import models.User;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
+
+    @BeforeMethod
+    public void precondition(){
+
+    }
+
 
     @Test
     public void loginPositive(){
@@ -14,14 +22,33 @@ public class LoginTests extends TestBase{
     @Test
     public void loginPositiveUser(){
 //    User user = new User("asd@fgh.com","$Asdf1234");
-        User user = new User();
+        User user = new User()
+                .withEmail("asd@fgh.com")
+                .withPassword("$Asdf1234")
+                ;
 //        user.setEmail("asd@fgh.com");
 //        user.setPassword("$Asdf1234");
-        user.setEmail("");
+//        user.setEmail("");
     app.getUser().openLoginForm();
         app.getUser().fillLoginForm(user.getEmail(), user.getPassword());
         app.getUser().submitLogin();
 
     }
+@Test
+    public void loginPositiveUserData(){
+        User user = new User()
+                .withEmail("asd@fgh.com")
+                .withPassword("$Asdf1234")
+                ;
 
+        app.getUser().openLoginForm();
+        app.getUser().fillLoginForm(user);
+        app.getUser().submitLogin();
+
+    }
+
+    @AfterMethod
+    public void postcondition(){
+
+    }
 }
