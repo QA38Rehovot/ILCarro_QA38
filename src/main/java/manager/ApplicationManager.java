291e78177fd30a2941074b49e200d5d84118cc99
webinartelran <webinartelran@gmail.com>
2023-07-04 20:29:21 +0300

    CW12

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -2,6 +2,7 @@ package manager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterSuite;
@@ -12,7 +13,9 @@ import java.util.concurrent.TimeUnit;
 public class ApplicationManager {
 
     Logger logger = LoggerFactory.getLogger(ApplicationManager.class);
-    WebDriver wd;
+//    WebDriver wd;
+
+    EventFiringWebDriver wd;
     HelperUser user;
     HelperCar car;
 
@@ -26,7 +29,9 @@ public class ApplicationManager {
 
     @BeforeSuite
     public void init(){
-        wd = new ChromeDriver();
+//        wd = new ChromeDriver();
+        wd = new EventFiringWebDriver(new ChromeDriver());
+        wd.register(new WebDriverListener());
         user = new HelperUser(wd);
         car = new HelperCar(wd);
 //        wd.manage().window().maximize();
