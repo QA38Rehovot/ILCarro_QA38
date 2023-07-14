1bd482dcc7653f3c82275de489d57f38e24d03ef
webinartelran <webinartelran@gmail.com>
2023-07-14 21:42:06 +0300

    CW15

--- src/test/java/RegistrationTests.java
+++ src/test/java/RegistrationTests.java
@@ -1,3 +1,4 @@
+import manager.ProviderData;
 import models.User;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -19,6 +20,25 @@ public class RegistrationTests extends TestBase {
                 .withLastName("Snow")
                 .withEmail("john_" + i + "@mail.com")
                 .withPassword("$Asdf1234");
+
+        app.getUser().openRegistrationForm();
+        logger.info("openRegistrationForm invoked");
+        app.getUser().fillRegistrationForm(user);
+        logger.info("fillRegistrationForm invoked");
+        app.getUser().submitLogin();
+        logger.info("submitLogin invoked");
+        logger.info("registrationPositive starts with credentials: login "
+                + user.getEmail() + " & password: " + user.getPassword());
+        Assert.assertTrue(app.getUser().isLoggedSuccess());
+    }
+    @Test(dataProvider = "userDtoCSV", dataProviderClass = ProviderData.class)
+    public void registrationPositiveDTO(User user){
+//        int i = (int)(System.currentTimeMillis()/1000)%3600;
+//        User user = new User()
+//                .withName("John")
+//                .withLastName("Snow")
+//                .withEmail("john_" + i + "@mail.com")
+//                .withPassword("$Asdf1234");
 
         app.getUser().openRegistrationForm();
         logger.info("openRegistrationForm invoked");
